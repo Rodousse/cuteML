1de1daea6c39173b86806145557c3ceb4348bb33
Robin Dousse <rdousse63@gmail.com>
2021-12-22 21:09:06 +0100

    [app] remove expression tree calls

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,5 @@
 #include "cuteml/File.hpp"
 
-#include <cuteml/ExpressionTree.hpp>
 #include <cuteml/fileIO.hpp>
 #include <iostream>
 
@@ -9,8 +8,6 @@ int main(int argc, char** argv)
     std::cout << "Hello friend\n";
     auto result = cuteml::IO::loadFileContent("./cuteml");
     std::cout << result.value_or("Arf not found...") << '\n';
-
-    std::cout << cuteml::flatten<cuteml::FlattenMethod::PreorderTraversal>().empty();
 
     cuteml::File myFile("./CMakeCache.txt");
     std::cout << myFile.format() << std::endl;
